1b160054ab94417859acd190086a65f1f2c35278
Mikhail Metrikin <metrikin@gmail.com>
2019-06-09 18:32:59 -0700

    Hooks example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [ userText, setUserText ] = useState('');
+
+  const updateUserText = (e) => {
+    setUserText(e.target.value);
+    console.log('current userText', userText);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Type Race</h2>
+      <input value={userText} onChange={updateUserText}/>
     </div>
   );
 }
