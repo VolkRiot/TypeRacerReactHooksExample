95418949da31072849c42f0922b176dc0d50eaa8
Mikhail Metrikin <metrikin@gmail.com>
2019-06-21 14:16:32 -0700

    Simple hooks example

--- src/App.js
+++ src/App.js
@@ -1,44 +1,53 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import getFailedSnapshotTests from 'jest-util/build/getFailedSnapshotTests';
 
 const App = () => {
+  const INITIAL_GAME_STATE = { victory: false, startTime: null, endTime: null };
   const SNIPPETS = [
-    "Bears, beets, battlestar galactica",
+    'Bears, beets, battlestar galactica',
     "What's Forrest Gump's password? 1Forrest1",
-    "Where do programmers like to hangout? The Foo Bar",
+    'Where do programmers like to hangout? The Foo Bar'
   ];
 
-  const [ userText, setUserText ] = useState('');
-  const [ snippet, setSnippet ] = useState('');
+  const [userText, setUserText] = useState('');
+  const [snippet, setSnippet] = useState('');
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
 
-  const updateUserText = (e) => {
+  const updateUserText = e => {
     setUserText(e.target.value);
-    console.log('current userText', userText);
-  }
+
+    if (e.target.value === snippet) {
+      setGameState({
+        ...gameState,
+        victory: true,
+        endTime: new Date().getTime() - gameState.startTime,
+      });
+    }
+  };
 
   const chooseSnippet = snippetIndex => () => {
-    console.log('setSnippet', snippetIndex);
     setSnippet(SNIPPETS[snippetIndex]);
-  }
+    setGameState({ ...gameState, startTime: new Date().getTime() });
+  };
 
   return (
     <div className="App">
       <h2>Type Race</h2>
-      <hr/>
+      <hr />
       <h3>Snippet</h3>
       {snippet}
-      <input value={userText} onChange={updateUserText}/>
-      <hr/>
-      {
-        SNIPPETS.map((snippet, index) => (
-          <button onClick={chooseSnippet(index)} key={index}>
-            {snippet.substring(0, 10)}
-          </button>
-        ))
-      }
+      <br />
+      <h4>{gameState.victory ? `Done! 🎉 Time: ${gameState.endTime}` : null}</h4>
+      <input value={userText} onChange={updateUserText} />
+      <hr />
+      {SNIPPETS.map((snippet, index) => (
+        <button onClick={chooseSnippet(index)} key={index}>
+          {snippet.substring(0, 10)}...
+        </button>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
